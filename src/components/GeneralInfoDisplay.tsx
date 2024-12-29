61ae853b1d373763512c81b05feb7f1cd2c29c95
MartinTintin3 <maroymart@gmail.com>
2024-12-29 14:14:13 -0500

    fixes

--- src/components/GeneralInfoDisplay.tsx
+++ src/components/GeneralInfoDisplay.tsx
@@ -1,10 +1,12 @@
-import { Card, Stack, Text, Title } from "@mantine/core";
+import { Card, Group, Paper, Stack, Text, Title } from "@mantine/core";
 import { GradeObject } from "../api/types/objects/grade";
 import { TeamAttributes, TeamObject } from "../api/types/objects/team";
 import { BoutObject } from "../api/types/objects/bout";
+import { Dayjs } from "dayjs";
 
 export type BasicInfo = {
 	name: string;
+	dateOfBirth?: Dayjs
 	grade?: GradeObject;
 	teams: {
 		attributes: TeamAttributes;
@@ -21,17 +23,35 @@ export default function GeneralInfoDisplay({ info }: Props) {
 		<Card p="xl" styles={{
 			root: {
 				border: "1px solid var(--mantine-color-gray-7)",
+				textAlign: "center",
 			}
 		}}>
-			<Title order={4} style={{ marginBottom: 0 }}>{info.name}</Title>
-			<Stack gap="sm">
-				<Text size="md">{info.grade ? info.grade.attributes.name : "Ungraded"}</Text>
-				{info.teams.map(team => (
-					<Stack key={team.attributes.identityTeamId} gap="sm">
-						<Text size="md">{team.attributes.name}</Text>
-						<Text size="sm">{team.matches.length} matches</Text>
-					</Stack>
-				))}
+			<Title order={3} style={{ marginBottom: 0 }}>{info.name}</Title>
+			<Stack gap={1} mt="sm">
+				<Stack m="md" gap={1}>
+					<Group gap={4} justify="center">
+						<Text size="md" fw={600}>Date of Birth: </Text>
+						<Text size="md">{info.dateOfBirth ? info.dateOfBirth.format("MMMM D, YYYY") : "Unknown"}</Text>
+					</Group>
+					<Group gap={4} justify="center">
+						<Text size="md" fw={600}>Grade: </Text>
+						<Text size="md">{info.grade ? info.grade.attributes.name : "Unknown"}</Text>
+					</Group>
+				</Stack>
+				<Stack gap="sm" justify="center">
+					<Text size="md" fw={600}>Registered Teams: </Text>
+					<Group gap="lg" justify="center">
+						{info.teams.map(team => (
+							<Paper p="sm" withBorder key={team.attributes.identityTeamId}>
+								<Stack gap={2}>
+									<Text size="md" fw="bold">{team.attributes.name}</Text>
+									<Text size="sm">{team.attributes.location.name}</Text>
+									<Text size="sm">{team.matches.length} matches</Text>
+								</Stack>
+							</Paper>
+						))}
+					</Group>
+				</Stack>
 			</Stack>
 		</Card>
 	);
