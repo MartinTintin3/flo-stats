aed3a12e008304065451969c8629b0ea82a4a16f
MartinTintin3 <maroymart@gmail.com>
2025-01-08 15:15:01 +0000

    oops

--- src/main.tsx
+++ src/main.tsx
@@ -53,14 +53,7 @@ ReactDOM.createRoot(root!).render(
 					margin: "2rem",
 				} }} size="lg" />*/}
 				<Routes>
-					<Route path="/" element={<>
-						<meta name="description" content="Search for athletes on FloWrestling" />
-						<meta name="keywords" content="flo, wrestling, search, athletes, match, results, stats" />
-						<meta name="author" content="Martin Maroyan" />
-						<meta name="og:title" content="FloStats" />
-						<meta name="og:description" content="Search for athletes on FloWrestling" />
-						<meta name="og:image" content="/favicon.ico" />
-					</>} />
+					<Route path="/" element={<></>} />
 					<Route path="/search" element={<SearchResultsPage />} />
 					<Route path="/athletes/:id" element={<Athletes />} />
 					<Route path="/teams/:id" element={<Athletes />} />
